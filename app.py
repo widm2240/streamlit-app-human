edb6207968ce13789959da458e555de84092ef19
widm2240 <widm@naver.com>
2022-11-30 15:29:04 +0900

    updated

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def main():
     iris_df = pd.read_csv(data/iris.csv)
     st.dataframe(iris_df)
     st.dataframe(iris_df, width=1000, height=200)
+
+    # 색상 추가
+    st.title('데이터 프레임에 색상')
+    st.dataframe(iris_df.style.highlight_max)
+
 if __name__ == "__main__":
     main()
