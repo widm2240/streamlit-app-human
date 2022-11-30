1755908aea92443863d34ad094658956ad777332
widm2240 <widm@naver.com>
2022-11-30 15:27:37 +0900

    updated

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import streamlit as st
 
 import pandas as pd
 
-main():
+def main():
     """코드작성"""
     iris_df = pd.read_csv(data/iris.csv)
     st.dataframe(iris_df)
+    st.dataframe(iris_df, width=1000, height=200)
 if __name__ == "__main__":
     main()
