87cc6d1e2f665f41ec1d2ffb75a96eacdc8ab91f
widm2240 <widm@naver.com>
2022-11-30 17:22:48 +0900

    updated

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 # -*- coding: utf-8 -*-
 
-# core pakg
+# core pkgs
 import streamlit as st
-# data pakg
+
+# data pkgs
 import pandas as pd
+
+# 이미지 라이브러리
+from PIL import Image
 
 def main():
     """코드작성"""
-    iris_df = pd.read_csv(data/iris.csv)
-    st.dataframe(iris_df)
-    st.dataframe(iris_df, width=1000, height=200)
+    # 텍스트 Input
+    fname = st.text_input('이름 입력')
+    st.write(fname)
+    
+    # 텍스트 영역
+    message = st.text_area('입력해주세용!', height = 100)
+    st.write(message)
 
-    # 색상 추가
-    st.title('데이터 프레임에 색상')
-    st.dataframe(iris_df.style.highlight_max(axis=1))
+    # 숫자 입력
+    number = st.number_input('숫자 입력')
+    st.write(number)
 
-    # 코드 보여주기
-    myCode = """
-    def hello():
-    print("Hello World")
-    end
-    """
+    # 날짜
+    myDate = st.date_input('날짜')
+    st.write(myDate)
+
+    # 시간
+    myTime = st.time_input('시간')
+    st.write(myTime)
+
+    #Color Picker
+    color = st.color_picker('색상 선택')
+    st.write(color)
+
 if __name__ == "__main__":
     main()
