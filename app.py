c083c55efee9a88b4a39cfd3c862074442b4b0ea
widm2240 <widm@naver.com>
2022-11-30 15:23:23 +0900

    updated

--- app.py
+++ app.py
@@ -2,29 +2,10 @@
 
 import streamlit as st
 
+import pandas as pd
 def main():
     """"코드작성"""
-    st.title("Hello World!")
-    st.subheader("임시...")
-
-    # text
-    num = 1
-    st.text(f'숫자는 {num}')
-
-    # 마크다운
-    st.markdown('## 마크다운 제목 2번째')
-
-    # 색상
-    st.success("성공")
-    st.warning("경고")
-    st.info("정보")
-    st.error("에러")
-    st.exception("예외")
-
-    # superfunction
-    st.write('문자열')
-    st.write(1+2)
-    st.write(dir(int))
-
+    iris_df = pd.read_csv(data/iris.csv)
+    
 if __name__ == "__main__":
     main()
